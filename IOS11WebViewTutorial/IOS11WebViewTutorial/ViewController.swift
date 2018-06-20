541333c6b0cd090a1a8dc78dbc75708ceddecf13
Evandro Martinelli <evandromartinelli@Evandros-MacBook-Air.local>
2018-06-20 00:44:39 -0300

    ajustes poc

--- IOS11WebViewTutorial/IOS11WebViewTutorial/ViewController.swift
+++ IOS11WebViewTutorial/IOS11WebViewTutorial/ViewController.swift
@@ -9,22 +9,49 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, WKNavigationDelegate {
+class ViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHandler {
+    
     var webView: WKWebView!
+    let userContentController = WKUserContentController()
     
     override func loadView() {
-        webView = WKWebView()
-        webView.navigationDelegate = self
+        super.loadView()
+        //webView.navigationDelegate = self
+        userContentController.add(self, name: "recaptcha")
+        let config = WKWebViewConfiguration()
+        config.userContentController = userContentController
+        webView = WKWebView(frame: self.view.bounds, configuration: config)
         view = webView
+    }
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        let dict = message.body as! [String:AnyObject]
+        
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //self.view.addSubview(self.webView)
+        do {
+            guard let filePath = Bundle.main.path(forResource: "test", ofType: "html")
+                else {
+                    // File Error
+                    print ("File reading error")
+                    return
+            }
+
+            let contents =  try String(contentsOfFile: filePath, encoding: .utf8)
+            let baseUrl = URL.init(string: "http://localhost") //URL(fileURLWithPath: filePath)
+            webView.loadHTMLString(contents as String, baseURL: baseUrl)
+        }
+        catch {
+            print ("File HTML error")
+        }
         
         // 1
-        let url = URL(string: "https://ioscreator.com")!
-        webView.load(URLRequest(url: url))
-        
+//        let url = URL(string: "https://www.userede.com.br/Acesso/Criacao/index.html")!
+//        webView.load(URLRequest(url: url))
+//
         // 2
         let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
         toolbarItems = [refresh]
